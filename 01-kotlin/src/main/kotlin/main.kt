c0ca9e965406d12b0364e565ce91475fb306f96f
Danna Feria <dannaferiav@gmail.com>
2023-12-02 20:22:25 -0500

    commit: variables en kotlin

--- 01-kotlin/src/main/kotlin/main.kt
+++ 01-kotlin/src/main/kotlin/main.kt
@@ -1,92 +1,204 @@
-abstract class Numeros(
-    protected  val numUno: Int,
-    protected val numDos: Int,
-){
-    init {
-        this.numUno; this.numDos;
-        numUno; numDos;
+import java.util.*
+
+fun main(){
+    println("Hola mundo");
+    // Tipos de variables
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable: String = "Danna";
+    // inmutable = "Vicente";
+
+    // Mutables (Re asignar)
+    var mutable: String = "Danna";
+    mutable = "Carolina";
+
+
+
+
+
+
+
+
+    //  val > var
+
+    // Duck Typing
+    var ejemploVariable = " Danna Feria "
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable = edadEjemplo;
+
+    // Variable primitiva
+    val nombreProfesor: String = "Danna Feria"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = true
+    // Clases Java
+    val fechaNacimiento: Date = Date()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // SWITCH
+    val estadoCivilWhen = "C"
+    when (estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" -> {
+            println("Soltero")
+        }
+        else -> {
+            println("No sabemos")
+        }
+    }
+    val coqueteo = if (estadoCivilWhen == "S") "Si" else "No"
+
+
+    calcularSueldo(10.00)
+    calcularSueldo(10.00, 15.00)
+    calcularSueldo(10.00, 12.00, 20.00)
+    calcularSueldo(sueldo = 10.00,  tasa = 12.00, bonoEspecial = 20.00) //  Parametros nombrados
+    calcularSueldo(10.00, bonoEspecial = 20.00) // Named Parameters
+    calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00) //  Parametros nombrados
+
+    val sumaUno = Suma(1,1)
+    val sumaDos = Suma(null, 1)
+    val sumaTres = Suma(1, null)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+abstract class NumerosJava{
+    protected val numeroUno: Int
+    private val numeroDos: Int
+    constructor(
+        uno: Int,
+        dos: Int
+    ){ // Bloque de codigo del constructor
+        this.numeroUno = uno
+        this.numeroDos = dos
         println("Inicializando")
     }
 }
 
-class Suma( //Constructor primario suma
-    uno: Int,
-    dos: Int
-): Numeros(uno, dos){ //LA CLASE SUMA TIENE A LA CLASE PADRE NUMEROS
-    init{
-        this.numUno; numUno;
-        this.numDos; numDos;
+abstract class Numeros( // Constructor PRIMARIO
+    // Ejemplo:
+    // uno: Int, // (Parametro (sin modificador de acceso))
+    // private var uno: Int, // Propiedad Publica Clase numeros.uno
+    // var uno: Int, // Propiedad de la clase (por defecto es PUBLIC)
+    // public var uno: Int,
+    // Propiedad de la clase protected numeros.numeroUno
+    protected val numeroUno: Int,
+    // Propiedad de la clase protected numeros.numeroDos
+    protected val numeroDos: Int,
+){
+    // var cedula: string = "" (public es por defecto)
+    // private valorCalculado: Int = 0 (private)
+    init {
+        this.numeroUno; this.numeroDos; // this es opcional
+        numeroUno; numeroDos; // sin el "this", es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+class Suma( // Constructor Primario Suma
+    uno: Int, // Parametro
+    dos: Int // Parametro
+) : Numeros(uno, dos) { // <- Constructor del Padre
+    init { // Bloque constructor primario
+        this.numeroUno; numeroUno;
+        this.numeroDos; numeroDos;
     }
 
-    constructor(
-        uno: Int?,
-        dos: Int
-    ): this(
-        if (uno==null)0 else uno, dos
-
-    ){
+    constructor(//  Segundo constructor
+        uno: Int?, // parametros
+        dos: Int // parametros
+    ) : this(  // llamada constructor primario
+        if (uno == null) 0 else uno,
+        dos
+    ) { // si necesitamos bloque de codigo lo usamos
         numeroUno;
     }
 
-
-
+    constructor(//  tercer constructor
+        uno: Int, // parametros
+        dos: Int? // parametros
+    ) : this(  // llamada constructor primario
+        uno,
+        if (dos == null) 0 else uno
+    ) // Si no lo necesitamos al bloque de codigo "{}" lo omitimos
 }
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-
-    //TIPOS DE VARIABLES
-    //- INMUTABLES (No se reasignan "=") *constantes*
-    val inmutable: String = "Adrian";
-    //-MUTABLES (reasignan)
-    var mutable: String ="Vicente";
-    mutable ="Danna"
-
-    //val>var
-    //DUCK TYPING
-    var ejemploVariable = "ADRIAN e"
-    val edadEjemplo: Int = 12
-    ejemploVariable.trim()
-
-    //VARIABLE PRIMITIVA
-    //val nombreProfe
-
-    //FUNCIONES
-    //VOID -> UNIT (no obligatorio)
-    fun imprimirNombre(nombre: String): Unit{
-        //TEMPLATE STRINGS
-
-        println("Nombre: ${nombre}")
-    }
-
-    fun calcularSueldo(
-        sueldo: Double,
-        tasa: Double = 12.00,
-        bonoEspecial: Double? = null,
-
-    ): Double{
-        if(bonoEspecial == null){
-            return  sueldo * (100/tasa)
-        }else{
-            bonoEspecial.dec()
-            return sueldo* (100/tasa) +bonoEspecial
-        }
-
-    }
-
-
-    calcularSueldo(10.00)
-    calcularSueldo(10.00, 12.00, 15.00)
-    calcularSueldo(10.00, 15.00)
-    //PARAMETROS NOMBRADOS
-    calcularSueldo(10.00, bonoEspecial = 20.00)
-    calcularSueldo(bonoEspecial = 14.00, sueldo = 5.00, tasa = 5.00)
-
-    val sumaUno = Suma(1,1)
-    val sumaDos = Suma(null,1)
-    val sumaTres = Suma(1,null)
 
 
 
+
+
+
+
+
+
+
+
+// void -> Unit
+fun imprimirNombre(nombre: String): Unit{
+    // template strings
+    // "Bienvenido: " + nombre + " "
+    println("Nombre : ${nombre}")
 }
+
+fun calcularSueldo(
+    sueldo: Double, // Requerido
+    tasa: Double = 12.00, // Opcional (defecto)
+    bonoEspecial: Double? = null, // Opcion null -> nullable
+): Double{
+    // Int -> Int? (nullable)
+    // String -> String? (nullable)
+    // Date -> Date? (nullable)
+    if(bonoEspecial == null){
+        return sueldo * (100/tasa)
+    }else{
+        bonoEspecial.dec()
+        return sueldo * (100/tasa ) + bonoEspecial
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
